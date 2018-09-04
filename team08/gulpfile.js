852b0fe56937781b434671937f2eedf3af0b46c6
htq <1492585850@qq.com>
2018-09-04 22:04:49 +0800

    八组完成

--- team08/gulpfile.js
+++ team08/gulpfile.js
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 var gulp = require('gulp')
 var server = require('gulp-webserver')
 var fs = require('fs')
@@ -7,6 +5,9 @@ var path = require('path')
 var url = require('url')
 var datajson = require('./src/data/data.json')
 var minjs = require('gulp-uglify')
+var minCss = require('gulp-clean-css');
+var concat = require('gulp-concat');
+var sass = require('gulp-sass')
 gulp.task('server', function() {
     return gulp.src('src')
         .pipe(server({
@@ -23,71 +24,22 @@ gulp.task('server', function() {
                 } else {
                     pathname = pathname === '/' ? '/index.html' : pathname
                     res.end(fs.readFileSync(path.join(__dirname, 'src', pathname)))
-=======
-var gulp = require('gulp');
-var minCss = require('gulp-clean-css');
-var uglify = require('gulp-uglify');
-var server = require('gulp-webserver');
-var concat = require('gulp-concat');
-var fs = require('fs');
-var path = require('path');
-var url = require('url');
-var sass = require('gulp-sass')
-gulp.task('css', function() {
-    return gulp.src('src/sass/*.scss')
-        .pipe(sass())
-        .pipe(gulp.dest('./src/css'))
-});
-gulp.task('server', function() {
-    return gulp.src('src')
-        .pipe(server({
-            port: 8090,
-            open: true,
-            middleware: function(req, res, next) {
-                var pathname = url.parse(req.url).pathname;
-                if (pathname === '/favicon.ico') {
-                    return false
-                }
-                if (pathname === '/api/src') {
-                    res.end('成功')
-                } else {
-                    pathname = pathname === '/' ? 'index.html' : pathname;
-                    res.end(fs.readFileSync(path.join(__dirname, 'src', pathname)));
->>>>>>> 04e5e74c3bd86506a0cd279227f663f639aba1a8
+
+
+                    gulp.task('css', function() {
+                        return gulp.src('src/sass/*.scss')
+                            .pipe(sass())
+                            .pipe(gulp.dest('./src/css'))
+                    });
+
+
+                    gulp.task('minjs', function() {
+                        return gulp.src('./src/js/*.js', '!./src/js/lib/*.js')
+                            .pipe(minjs())
+                            .pipe(gulp.dest('build'))
+                    })
+                    gulp.task('dev', gulp.series('minjs', 'server'));
                 }
             }
         }))
 })
-<<<<<<< HEAD
-gulp.task('minjs', function() {
-    return gulp.src('./src/js/*.js', '!./src/js/lib/*.js')
-        .pipe(minjs())
-        .pipe(gulp.dest('build'))
-})
-gulp.task('dev', gulp.series('minjs', 'server'))
-=======
-gulp.task('dev', gulp.series(['css', 'server']))
->>>>>>> 04e5e74c3bd86506a0cd279227f663f639aba1a8
-=======
-var gulp = require('gulp');
-var server = require('gulp-webserver');
-var path = require('path');
-var fs = require('fs');
-var url = require('url');
-gulp.task('dev', function() {
-    return gulp.src('src')
-        .pipe(server({
-            port: 8520,
-            open: true,
-            middleware: function(req, res, next) {
-                var pathname = url.parse(req.url).pathname
-                if (pathname === '/favicon.ico') {
-                    res.end('')
-                    return
-                }
-                var pathname = pathname === '/' ? 'index.html' : pathname
-                res.end(fs.readFileSync(path.join(__dirname, 'src', pathname)))
-            }
-        }))
-})
->>>>>>> e689e1a37ffe3427fccfb09e905c8ed01051424b
