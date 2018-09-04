e689e1a37ffe3427fccfb09e905c8ed01051424b
ZJF <993537879@qq.com>
2018-09-04 21:37:58 +0800

    scroll

---
+++ team08/gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp');
+var server = require('gulp-webserver');
+var path = require('path');
+var fs = require('fs');
+var url = require('url');
+gulp.task('dev', function() {
+    return gulp.src('src')
+        .pipe(server({
+            port: 8520,
+            open: true,
+            middleware: function(req, res, next) {
+                var pathname = url.parse(req.url).pathname
+                if (pathname === '/favicon.ico') {
+                    res.end('')
+                    return
+                }
+                var pathname = pathname === '/' ? 'index.html' : pathname
+                res.end(fs.readFileSync(path.join(__dirname, 'src', pathname)))
+            }
+        }))
+})
